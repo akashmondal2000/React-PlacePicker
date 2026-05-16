db37fb7e817afd41dbb6487d5c3928f3135eefd2
akashmondal2000 <mondalakash494@gmail.com>
2026-05-16 14:07:14 +0530

    get places data using api calling from backend

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,29 @@
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFatching,setIsFatching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+
+    async function fetchPlaces(){
+      setIsFatching(true)
+      const responce = await fetch("http://localhost:3000/places");
+      const resData = await responce.json();
+      setAvailablePlaces(resData.places);
+      setIsFatching(false)
+    }
+
+    fetchPlaces()
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading = {isFatching}
+      loadingText = "Fatching place data...."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
