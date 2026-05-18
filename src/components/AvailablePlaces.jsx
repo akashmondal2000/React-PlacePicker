b9bb8e662efbfbf670e87f8b8e47f380ae26b326
akashmondal2000 <mondalakash494@gmail.com>
2026-05-18 11:12:30 +0530

    fetch data , fix

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,29 +1,54 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isFatching,setIsFatching] = useState(false);
+  const [isFatching, setIsFatching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
+    async function fetchPlaces() {
+      setIsFatching(true);
 
-    async function fetchPlaces(){
-      setIsFatching(true)
-      const responce = await fetch("http://localhost:3000/places");
-      const resData = await responce.json();
-      setAvailablePlaces(resData.places);
-      setIsFatching(false)
+      try {
+       const places = await fetchAvailablePlaces();
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.longitude,
+            position.coords.latitude,
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFatching(false);
+        });
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fatch Places, Please try again letter",
+        });
+        setIsFatching(false);
+      }
+
+      
     }
 
-    fetchPlaces()
+    fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title={"An error occored!"} message={error.message} />;
+  }
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
-      isLoading = {isFatching}
-      loadingText = "Fatching place data...."
+      isLoading={isFatching}
+      loadingText="Fatching place data...."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
